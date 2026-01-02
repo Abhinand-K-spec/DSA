eebe536ba45ac5d8b65e0fe0ae1378f3af0c5f3c
Abhinand k <abhinnanndk@gmail.com>
2026-01-02 11:00:10 +0530

    hashtble

--- dsa 2/hashTable.js
+++ dsa 2/hashTable.js
@@ -1,58 +1,89 @@
-class hashTable{
-    constructor(size=10){
-        this.table = new Array(size);
-        this.size = size;
-    }
+// class hashTable{
+//     constructor(size=10){
+//         this.table = new Array(size);
+//         this.size = size;
+//     }
 
-    _hash(key){
-        let total = 0;
-        for(let ch of key){
-            total += ch.charCodeAt(0);
-        }
-        return total % this.size;
-    }
+//     _hash(key){
+//         let total = 0;
+//         for(let ch of key){
+//             total += ch.charCodeAt(0);
+//         }
+//         return total % this.size;
+//     }
 
-    set(key,val){
-        let index = this._hash(key);
-        if(!this.table[index]){
-            this.table[index] = [];
-        }
+//     set(key,val){
+//         let index = this._hash(key);
+//         if(!this.table[index]){
+//             this.table[index] = [];
+//         }
 
-        for(let pair of this.table[index]){
-            if(pair[0]===key){
-                pair[1] = val;
-                return;
-            }
-        }
-        this.table[index].push([key,val]);
-    }
+//         for(let pair of this.table[index]){
+//             if(pair[0]===key){
+//                 pair[1] = val;
+//                 return;
+//             }
+//         }
+//         this.table[index].push([key,val]);
+//     }
 
-    get(key){
-        let index = this._hash(key);
-        let bucket = this.table[index];
+//     get(key){
+//         let index = this._hash(key);
+//         let bucket = this.table[index];
 
-        if(!bucket) return null;
-        for(let pair of bucket){
-            if(pair[0]===key){
-                return pair[1];
-            }
-        }
-    }
+//         if(!bucket) return null;
+//         for(let pair of bucket){
+//             if(pair[0]===key){
+//                 return pair[1];
+//             }
+//         }
+//     }
 
-    remove(key){
-        let index = this._hash(key);
-        let bucket = this.table[index];
-        for(let i=0;i<bucket.length;i++){
-            if(bucket[i][0]===key){
-                bucket.splice(i,1);
-                return;
-            }
-        }
-        return null;
-    }
-}
+//     remove(key){
+//         let index = this._hash(key);
+//         let bucket = this.table[index];
+//         for(let i=0;i<bucket.length;i++){
+//             if(bucket[i][0]===key){
+//                 bucket.splice(i,1);
+//                 return;
+//             }
+//         }
+//         return null;
+//     }
+// }
 
 
-let ht = new hashTable();
-ht.set('name','abhi');
-ht.set('age',20);
+// let ht = new hashTable();
+// ht.set('name','abhi');
+// ht.set('age',20);
+
+
+
+
+
+// // hashing with probing (linear)--------------------------
+
+// class hashTable{
+//     constructor(size = 10){
+//         this.table = new Array(size);
+//         this.size = size;
+//     }
+
+
+//     hash(key){
+//         let total = 0;
+//         for(let ch of key){
+//             total += ch.charCodeAt(0);
+//         }
+//         return total % this.size;
+//     }
+
+//     set(key,val){
+//         let index = this.hash(key);
+//         while(this.table[index]){
+//             index = (index+1) % this.size;
+//         }
+//         this.table[index] = [key,val];
+
+//     }
+// }
