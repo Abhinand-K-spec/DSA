1c02a8dac1a53dce3eea5ed7fe30846a90b70090
Abhinand k <abhinnanndk@gmail.com>
2026-01-07 13:14:54 +0530

    heap

--- dsa 3/bst.js
+++ dsa 3/bst.js
@@ -35,7 +35,39 @@ class BST{
             }else{return} 
         }
     }
+
+
+
+    deleteElem(value){
+        this.root = this.remove(this.root,value);
+    }
+
+    remove(node,value){
+        if(!node)return null;
+        if(node.value>value){
+            node.left = this.remove(node.left,value);
+        }else if(node.value<value){
+            node.right = this.remove(node.right,value);
+        }else{
+            if(!node.left) return node.right;
+            if(!node.right) return node.left;
+
+            let min = this.minNode(node.right);
+            node.value = min.value;
+            node.right = this.remove(node.right,min.value);
+        }
+
+        return node;
+    }
     
+
+    minNode(node){
+        if(!node) return null;
+        while(node.left){
+            node = node.left;
+        }
+        return node;
+    }
     
     inorder(node = this.root){
         if(!node) return;
@@ -57,7 +89,33 @@ class BST{
         this.postorder(node.right);
         console.log(node.value);
     }
+
+
+    closest(target){
+        let current = this.root;
+        let closest = this.root.value;
+        while(current){
+            if(Math.abs(target - current.value) < Math.abs(target - closest.value)){
+                closest = current.value;
+            }if(target<current.value){
+                current = current.left;
+            }else if(target>current.value){
+                current = current.right;
+            }else{
+                return current.value;
+            }
+        }return closest;
+    }
     
+
+    isValid(node=this.root,min = -Infinity, max = Infinity){
+        if(!node) return true;
+        if(node.value<=min || node.value>=max){
+            return false;
+        }
+        return this.isValid(node.left,min,node.value) && 
+        this.isValid(node.right,node.value,max);
+    }
     
 }
 
@@ -68,6 +126,8 @@ bst.insert(9);
 bst.insert(6);
 bst.insert(17);
 bst.insert(12);
-bst.postorder()
+bst.delete(9)
+console.log(bst.isValid())
+
 
 
---
+++ dsa 3/heap.js
@@ -0,0 +1,67 @@
+class minHeap{
+    constructor(){
+        this.heap = [];
+    }
+
+    insert(value){
+        this.heap.push(value);
+        this.heapify();
+    }
+
+
+    heapify(){
+        let index = this.heap.length-1;
+        while(index>0){
+            let parentIndex = Math.floor((index-1)/2);
+
+            if(this.heap[parentIndex] < this.heap[index]) break;
+            [this.heap[parentIndex],this.heap[index]] = [this.heap[index],this.heap[parentIndex]];
+
+            index = parentIndex;
+        }
+    }
+
+    removeMin(){
+        if(this.heap.length===0) return null;
+        if(this.heap.length===1) return this.heap.pop();
+
+        let min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+        return min;
+    }
+
+    heapifyDown(){
+        let index = 0;
+        while(true){
+            let left = (2*index) +1;
+            let right  = (2*index) +2;
+            let smallest = index;
+
+            if(left<this.heap.length && this.heap[left]<this.heap[smallest] ){
+                smallest = left;
+            }if(right,this.heap.length && this.heap[right]<this.heap[smallest]){
+                smallest = right;
+            }if(smallest === index) break;
+
+            [this.heap[index],this.heap[smallest]] = [this.heap[smallest]< this.heap[index]];
+
+            index = smallest;
+        }
+    }
+
+    display(){
+        console.log(this.heap)
+    }
+}
+
+
+
+let h = new minHeap();
+h.insert(8);
+h.insert(9);
+h.insert(7);
+h.insert(5);
+h.insert(19);
+h.insert(0);
+h.display()
